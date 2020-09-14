5eede4f610f2f50a9443b535c2462db16aff7ca3
이용홍 <hong@pharmcadd.com>
2020-09-14 09:57:04 +0900

    fix. combine protein ligand 시 벡터 좌표 default 는 ligand 의 벡터 좌표로 설정

--- src/main/kotlin/pharmcadd/gromacs/gromacs.kt
+++ src/main/kotlin/pharmcadd/gromacs/gromacs.kt
@@ -14,6 +14,9 @@ fun combineProteinLigand(protein: String, ligand: String): String {
     for (s in lig.subList(2, lig.size - 1)) {
         complex.add(s)
     }
-    complex.add(pro.last())
+
+    // 멤브레인인 경우에는 벡터 좌표가 따로 들어 가기 때문에 아무 값이나 들어 가도 상관없다고 한다. by 이상백 박사님
+    // 솔루션인 경우에는......? 솔루션 빌더 붙일 때 확인
+    complex.add(lig.last())
     return complex.joinToString("\n")
 }
